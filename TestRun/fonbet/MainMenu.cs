80b048ee23ec5c0e26d1c9a3b4b5a8ea747e64b3
nmashkov <ieatpeople99@yandex.ru>
2018-05-24 15:47:23 +0300

    Добавлен тест Slider

--- TestRun/fonbet/MainMenu.cs
+++ TestRun/fonbet/MainMenu.cs
@@ -50,66 +50,51 @@ namespace TestRun.fonbet
                 throw new Exception("Нет ссылки на \"Как играть на ставкомате\"");
         }
     }
-    //class Slider : FonbetWebProgram
-    //{
-    //    public static CustomProgram FabricateSlider()
-    //    {
-    //        return new Slider();
-    //    }
+    class Slider : FonbetWebProgram
+    {
+        public static CustomProgram FabricateSlider()
+        {
+            return new Slider();
+        }
 
-    //    protected override bool NeedLogin()
-    //    {
-    //        return false;
-    //    }
+        protected override bool NeedLogin()
+        {
+            return false;
+        }
 
-    //    public override void Run()
-    //    {
-    //        base.Run();
+        public override void Run()
+        {
+            base.Run();
 
-    //        if (!WebElementExist(".//*[@class='how2play']"))
-    //            throw new Exception("Нет виджета HowToPlay");
+            if (!WebElementExist(".//*[@class='home-slider__switch-wrap']/div[1]"))
+                throw new Exception("На главной странице нет слайдера");
 
-    //        LogStage("Проверка работы всех разделов меню \"Я хочу заключать пари в интернете\"");
-    //        ClickWebElement(".//*[@class='how2play__help-steps']/div[1]", "Кнопка как заключать пари в интернете", "кнопки как заключать пари в интернете");
-    //        ClickWebElement(".//*[@class='how2play__stepout _visible']/div[1]", "Раздел удаленная идентификация", "раздела удаленная идентификация");
-    //        if (!WebElementExist(".//*[@class='how2play__stepout _visible']/div[1]//*[@href='#!/account/registration/RemoteVerification2']"))
-    //            throw new Exception("Нет ссылки на удаленную верификацию");
+            LogStage("Проверка переключения слайдера по кнопкам");
+            ClickWebElement(".//*[@class='home-slider__switch-wrap']/div[1]", "Кнопка первой страницы слайдера", "кнопки первой страницы слайдера");
+            var sliderButton = GetWebElement(".//*[@class='home-slider__switch-wrap']/div[1]", "Нет кнопки у слайдера");
+            var sliderButtonClass = sliderButton.GetAttribute("class");
+            if (!sliderButtonClass.Contains("selected"))
+                throw new Exception("Проблемы со слайдером на главной странице");
+            ClickWebElement(".//*[@class='home-slider__switch-wrap']/div[2]", "Кнопка второй страницы слайдера", "кнопки второй страницы слайдера");
+            var sliderSecondButton = GetWebElement(".//*[@class='home-slider__switch-wrap']/div[2]", "У слайдера только одна кнопка");
+            var sliderSecondButtonClass = sliderSecondButton.GetAttribute("class");
+            if (!sliderSecondButtonClass.Contains("selected"))
+                throw new Exception("Проблемы со слайдером на главной странице");
 
-    //        Setup(browserName);
-    //        driver.FindElement(By.XPath(".//*[@class='home-slider__switch-wrap']/div[1]")).Click();
-    //        Assert.True(driver.FindElement(By.XPath(".//*[@class='home-slider__switch-wrap']/div[1]")).GetAttribute("class").Contains("selected"), "Проблемы со слайдером на главной странице");
-    //        driver.FindElement(By.XPath(".//*[@class='home-slider__switch-wrap']/div[2]")).Click();
-    //        Assert.True(driver.FindElement(By.XPath(".//*[@class='home-slider__switch-wrap']/div[2]")).GetAttribute("class").Contains("selected"), "Проблемы со слайдером на главной странице");
-    //        driver.FindElement(By.XPath(".//*[@class='home-slider__bets']/div[1]")).Click();
-    //        Assert.True(IsElementPresent(".//*[@class='authorization__inner']"), "Не появилось окно авторизации");
-    //        driver.FindElementWait(By.XPath(".//*[@class='login-form__form']/div[1]/input"), 6).SendKeys("11");
-    //        driver.FindElement(By.XPath(".//*[@class='login-form__form']/div[2]/input")).SendKeys("ueueue11");
-    //        driver.FindElement(By.XPath(".//*[@class='login-form__form-row _right']/div[2]/button")).Click();
-    //        // Thread.Sleep(1000);
-    //        // driver.FindElementWait(By.XPath(".//*[@class='home-slider__switch-wrap']/div[2]"),8).Click();
-    //        waitElementIsVisible(driver, By.XPath(".//*[@class='home-slider__bets']/div[1]"));
-    //        driver.FindElement(By.XPath(".//*[@class='home-slider__bets']/div[1]")).Click();
-    //        Assert.True(IsElementPresent(".//*[@class='coupons']/div[1]//*[@class='coupon__title']"), "Не появилась новая ставка");
-
-
-    //        ClickWebElement(".//*[@class='how2play__stepout _visible']/div[2]", "Раздел карты Фонбет", "раздела карты Фонбет");
-    //        if (!WebElementExist(".//*[@class='how2play__stepout _visible']/div[2]//*[@href='#!/account/registration/Bk']"))
-    //            throw new Exception("Нет ссылки на регистрацию БК");
-
-    //        ClickWebElement(".//*[@class='how2play__stepout _visible']/div[3]", "Раздел кошелек Киви", "раздела кошелек Киви");
-    //        if (!WebElementExist(".//*[@class='how2play__stepout _visible']/div[3]//*[@href='#!/account/registration/Qiwi']"))
-    //            throw new Exception("Нет ссылки на регистрацию Киви");
-
-    //        ClickWebElement(".//*[@class='how2play__stepout _visible']/div[4]", "Раздел без карты и без киви", "раздела без карты и без киви");
-    //        if (!WebElementExist(".//*[@class='how2play__stepout _visible']/div[4]//*[@href='#!/account/how2Register']"))
-    //            throw new Exception("Нет ссылки на  ИНУЮ регистрацию");
-
-    //        LogStage("Проверка работы всех разделов меню \"Я хочу заключать пари в клубах\"");
-    //        ClickWebElement(".//*[@class='how2play__help-steps']/div[2]", "Кнопка как заключать пари в клубах", "кнопки как заключать пари в клубах");
-    //        if (!WebElementExist("//*[@href='#!/products/addresses']"))
-    //            throw new Exception("Нет ссылки на найти клубы");
-    //        if (!WebElementExist("//*[@href='#!/pages/fonapps']"))
-    //            throw new Exception("Нет ссылки на \"Как играть на ставкомате\"");
-    //    }
-    //}
+            LogStage("Проверка ставки через слайдер");
+            ClickWebElement(".//*[@class='home-slider__bets']/div[1]", "Модуль ставки на странице слайдера", "модуля ставки на странице сладера");
+            if (!WebElementExist(".//*[@class='authorization__inner']"))
+                throw new Exception("Не появилось окно авторизации");
+            SendKeysToWebElement(".//*[@class='login-form__form']/div[1]/input", Login, "поле логина", "поля логина");
+            SendKeysToWebElement(".//*[@class='login-form__form']/div[2]/input", Password, "поле пароля", "поля пароля");
+            ClickWebElement(".//*[@class='login-form__form-row _right']/div[2]/button", "Кнопка логина", "кнопки логина");
+            ClickWebElement(".//*[@class='home-slider__switch-wrap']/div[2]", "Кнопка второй страницы слайдера", "кнопки второй страницы слайдера");
+            ClickWebElement(".//*[@class='home-slider__bets']/div[1]", "Модуль ставки на странице слайдера", "модуля ставки на странице сладера");
+            if (!WebElementExist(".//*[@class='coupons']/div[1]//*[@class='coupon__title']"))
+                throw new Exception("Не появилась новая ставка");
+            //  waitElementIsVisible(driver, By.XPath(".//*[@class='home-slider__bets']/div[1]"));
+          
+           
+        }
+    }
 }
